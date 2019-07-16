a06c4b0e1132dab731a4daeadab257bdfaf40eb8
xzxxcx <915962319@qq.com>
2019-07-16 16:29:31 +0800

    login

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
-  app组件
+  <!-- app组件 -->
+  <router-view></router-view>
   </div>
 </template>
 
---
+++ src/views/Login/index.vue
@@ -0,0 +1,28 @@
+<template>
+<div class="login">
+    <div class="login-box">
+登录
+    </div>
+</div>
+</template>
+<style lang="less" scoped>
+.login{
+    height: 100%;
+    background: #2b4b6b;
+}
+.login-box{
+    width: 450px;
+    height: 300px;
+    background-color: #fff;
+    border-radius: 3px;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%,-50%)
+}
+</style>
+<script>
+export default {
+
+}
+</script>
